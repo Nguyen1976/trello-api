f231409575d4e84afc9563ef381e1d9df4086119
Nguyen1976 <nguyen2202794@gmail.com>
2025-03-04 17:42:32 +0700

    add MongoDB connection and update server to use database

--- src/config/mongodb.js
+++ src/config/mongodb.js
@@ -3,3 +3,36 @@
  * YouTube: https://youtube.com/@trungquandev
  * "A bit of fragrance clings to the hand that gives flowers!"
  */
+
+const MONGODB_URI =
+  'mongodb+srv://nguyen22027904:nguyen1976@trello-web.focnq.mongodb.net/?retryWrites=true&w=majority&appName=Trello-web'
+const DATABASE_NAME = 'trello-web'
+
+import { MongoClient, ServerApiVersion } from 'mongodb'
+
+let trelloDatabaseInstance = null
+
+//Khời tạo 1 đói tượng ClientInstance để connect tới mongodb
+const mongoClientInstance = new MongoClient(MONGODB_URI, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true
+  }
+})
+
+export const CONNECT_DB = async () => {
+  await mongoClientInstance.connect()
+
+  trelloDatabaseInstance = mongoClientInstance.db(DATABASE_NAME)
+}
+
+//Có tác dụng export trelloDatabaseInstance khi đã connect thành công và có thể dùng ở nhiều nơi khác nhau
+export const GET_DB = () => {
+  if (!trelloDatabaseInstance) throw new Error('Call connectDB first!')
+  return trelloDatabaseInstance
+}
+
+export const CLOSE_DB = async () => {
+  await mongoClientInstance.close()
+}
