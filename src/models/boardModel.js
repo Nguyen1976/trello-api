ee036b317c833d06c92c7cb26b68edd08ba0ce09
Nguyen1976 <nguyen2202794@gmail.com>
2025-03-05 18:05:17 +0700

    validate board data before creation; use ObjectId for finding board by id

--- src/models/boardModel.js
+++ src/models/boardModel.js
@@ -1,4 +1,5 @@
 import Joi from 'joi'
+import { ObjectId } from 'mongodb'
 import { OBJECT_ID_RULE, OBJECT_ID_RULE_MESSAGE } from '~/utils/validators'
 import { GET_DB } from '~/config/mongodb'
 
@@ -21,11 +22,18 @@ const BOARD_COLLECTION_SCHEMA = Joi.object({
   _destroy: Joi.boolean().default(false)
 })
 
+const validateBeforeCreate = async (data) => {
+  return await BOARD_COLLECTION_SCHEMA.validateAsync(data, {
+    abortEarly: false
+  })
+}
+
 const createNew = async (data) => {
   try {
+    const validData = await validateBeforeCreate(data)
     const createdBoard = await GET_DB()
       .collection(BOARD_COLLECTION_NAME)
-      .insertOne(data)
+      .insertOne(validData)
     return createdBoard
   } catch (error) {
     throw new Error(error)
@@ -34,9 +42,11 @@ const createNew = async (data) => {
 
 export const findOneById = async (id) => {
   try {
-    const result = await GET_DB().collection(BOARD_COLLECTION_NAME).findOne({
-      _id: id
-    })
+    const result = await GET_DB()
+      .collection(BOARD_COLLECTION_NAME)
+      .findOne({
+        _id: new ObjectId(id)
+      })
 
     return result
   } catch (error) {
