a36725ac0d43cdc55fbf0e41f06eba8c22b33562
Nguyen1976 <nguyen2202794@gmail.com>
2025-03-05 15:38:27 +0700

    add environment variables for APP_PORT and BUILD_MODE; update clean script to use rimraf

--- src/middlewares/errorHandlingMiddleware.js.js
+++ src/middlewares/errorHandlingMiddleware.js.js
@@ -3,17 +3,16 @@
  * YouTube: https://youtube.com/@trungquandev
  * "A bit of fragrance clings to the hand that gives flowers!"
  * NOTE: (Muốn hiểu rõ hơn về code trong file này thì vui lòng xem video 55 trong bộ MERN Stack trên kênh Youtube của mình.)
-*/
-
+ */
 
 //https://github.com/trungquandev/trungquandev-public-utilities-algorithms/blob/main/12-node-expressjs-handling-errors/errorHandlingMiddleware.js
 
 /* eslint-disable no-unused-vars */
 import { StatusCodes } from 'http-status-codes'
+import { env } from '~/config/environment'
 
 // Middleware xử lý lỗi tập trung trong ứng dụng Back-end NodeJS (ExpressJS)
 export const errorHandlingMiddleware = (err, req, res, next) => {
-
   // Nếu dev không cẩn thận thiếu statusCode thì mặc định sẽ để code 500 INTERNAL_SERVER_ERROR
   if (!err.statusCode) err.statusCode = StatusCodes.INTERNAL_SERVER_ERROR
 
@@ -26,7 +25,7 @@ export const errorHandlingMiddleware = (err, req, res, next) => {
   // console.error(responseError)
 
   // Chỉ khi môi trường là DEV thì mới trả về Stack Trace để debug dễ dàng hơn, còn không thì xóa đi. (Muốn hiểu rõ hơn hãy xem video 55 trong bộ MERN Stack trên kênh Youtube: https://www.youtube.com/@trungquandev)
-  // if (env.BUILD_MODE !== 'dev') delete responseError.stack
+  if (env.BUILD_MODE !== 'dev') delete responseError.stack
 
   // Đoạn này có thể mở rộng nhiều về sau như ghi Error Log vào file, bắn thông báo lỗi vào group Slack, Telegram, Email...vv Hoặc có thể viết riêng Code ra một file Middleware khác tùy dự án.
   // ...
