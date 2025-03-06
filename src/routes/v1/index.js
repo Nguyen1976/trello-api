247a89707d5a5b56124cc570774596272390b622
Nguyen1976 <nguyen2202794@gmail.com>
2025-03-06 18:19:16 +0700

    implement card creation functionality with validation and routing(part 1)

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,6 +1,8 @@
 import express from 'express'
 import { StatusCodes } from 'http-status-codes'
 import { boardRoute } from './boardRoute'
+import { columnRoute } from './columnRoute'
+import { cardRoute } from './cardRoute'
 
 const Router = express.Router()
 
@@ -11,5 +13,7 @@ Router.get('/status', (req, res) => {
 
 /*Board API */
 Router.use('/boards', boardRoute)
+Router.use('/columns', columnRoute)
+Router.use('/cards', cardRoute)
 
 export const APIs_v1 = Router
