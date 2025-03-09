280ace5400de0b499efc2fe8bdcad24b8db67745
Nguyen1976 <nguyen2202794@gmail.com>
2025-03-09 23:13:04 +0700

    refactor CORS configuration to simplify local development setup

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -5,7 +5,10 @@
  */
 
 //Những domain được phép truy cập tới tài nguyên của server
-export const WHITELIST_DOMAINS = ['http://localhost:5173']
+export const WHITELIST_DOMAINS = [
+  // 'http://localhost:5173'//Không cần localhost nữ vì ở file config/cors đã luôn cho phép môi trường dev
+
+]
 
 export const BOARD_TYPES = {
   PUBLIC: 'public',
